e7e51d074cb230e1ba9456ab723fff54f4447788
Yuki Fujiwara <sky.y.0079@gmail.com>
2012-08-18 17:44:57 +0900

    * Made module XMorgDown * Changed action when option is nothing

--- XMorgDown.rb
+++ XMorgDown.rb
@@ -4,111 +4,125 @@
 require "optparse"
 require 'pp'
 
-DEBUG = true
+module XMorgDown
 
-class Core
+  DEBUG = true
 
-  def initialize()
-    
-  end
+  class Core
 
-  def parse()
-    
-  end
-
-  def export()
-    
-  end
-end
-
-
-class Exporter
-  attr_reader :html
-
-  def initialize(html)
-    @html = html
-  end
-
-end
-
-
-
-class Command
-  attr_reader :option,:argv
-  
-  def initialize(argv)
-    @argv = argv
-    @option = { }
-    @option[:format_from] = 'html' # fixed
-    @option[:format_to] = 'markdown' # default
-    @option[:file_input] = ''
-    @option[:file_output] = ''
-  end
-
-  # Parse command line options
-  def parse()
-    # Option Parser of optparse
-    opt = OptionParser.new
-    script_name = File.basename($0)
-    opt.banner = "Usage: ruby #{script_name} [options] input.xmind"
-    
-    #
-    # Option Settings
-    #
-    
-    # Output
-    opt.on('-o FILE','--output FILE','Output Filename') {|v| @option[:file_output] = v }
-    
-    # Format(Output)
-    output_formats_short = %w(markdown[default] org html latex rst plain)
-    output_formats_long = %w(native json html html5 html+lhs html5+lhs s5 slidy slideous dzslides docbook opendocument latex latex+lhs beamer beamer+lhs context texinfo man markdown markdown+lhs plain rst rst+lhs mediawiki textile rtf org asciidoc odt docx epub)
-    
-    str_output_formats = "Output formats: " + output_formats_short.join(', ') + ", ... \n\t\t\t\t\t(for other formats: see pandoc --help)"
-    
-    opt.on('-t FORMAT','--to=FORMAT',str_output_formats) {|v| @option[:format_to] = v }
-    opt.on('-w FORMAT','--write=FORMAT',str_output_formats) {|v| @option[:format_to] = v }
-    
-    
-    # Help
-    opt.on( '-h', '--help', 'Display this screen' ) do
-      puts opt
-      exit
+    def initialize()
+      
     end
 
-    
-    # Do parsing
-    begin
-      pp @argv if DEBUG
-      opt.permute!(@argv)
-    rescue => e
-      # Catch exception when an option is wrong
-      puts opt
-      abort(e)
+    def parse()
+      
     end
-    
-    @option[:file_input] = @argv.pop
 
-    ## Check whether the filename ends with ".xmind"
-    if not (@option[:file_input] =~ /^.*\.xmind$/)
-      puts opt
-      abort("ERROR: filename should end with \".xmind\"")
+    def export()
+      
     end
+  end
+
+
+  class Exporter
+    attr_reader :html
+
+    def initialize(html)
+      @html = html
+    end
+
+  end
+
+
+
+  class Command
+    attr_reader :option,:argv
     
+    def initialize(argv)
+      @argv = argv
+      @option = { }
+      @option[:format_from] = 'html' # fixed
+      @option[:format_to] = 'markdown' # default
+      @option[:file_input] = ''
+      @option[:file_output] = ''
+    end
+
+    # Parse command line options
+    def parse()
+      # Option Parser of optparse
+      opt = OptionParser.new
+      script_name = File.basename($0)
+      opt.banner = "Usage: ruby #{script_name} [options] input.xmind"
+      
+      #
+      # Option Settings
+      #
+      
+      # Output
+      opt.on('-o FILE','--output FILE','Output Filename') {|v| @option[:file_output] = v }
+      
+      # Format(Output)
+      output_formats_short = %w(markdown[default] org html latex rst plain)
+      output_formats_long = %w(native json html html5 html+lhs html5+lhs s5 slidy slideous dzslides docbook opendocument latex latex+lhs beamer beamer+lhs context texinfo man markdown markdown+lhs plain rst rst+lhs mediawiki textile rtf org asciidoc odt docx epub)
+      str_output_formats = "Output formats: " + output_formats_short.join(', ') + ", ... \n\t\t\t\t\t(for other formats: see pandoc --help)"
+      
+      opt.on('-t FORMAT','--to=FORMAT',str_output_formats) {|v| @option[:format_to] = v }
+      opt.on('-w FORMAT','--write=FORMAT',str_output_formats) {|v| @option[:format_to] = v }
+
+      # Help
+      opt.on( '-h', '--help', 'Display this screen' ) do
+        puts opt
+        exit
+      end
+
+      
+      # Do parsing
+      begin
+        pp @argv if DEBUG
+        opt.permute!(@argv)
+      rescue => e
+        # Catch exception when an option is wrong
+        puts opt
+        abort(e)
+      end
+
+      ## Check whether ARGV is null
+      if @argv.length == 0
+        # Show help
+        puts opt
+        exit
+      end
+      
+      @option[:file_input] = @argv.pop
+
+      ## Check whether the filename ends with ".xmind"
+      if not (@option[:file_input] =~ /^.*\.xmind$/)
+        puts opt
+        abort("ERROR: filename should end with \".xmind\"")
+      end
+      
+      # Check format
+      if not output_formats_long.include?(@option[:format_to])
+        puts opt
+        abort("ERROR: wrong output format: "+ @option[:format_to])
+      end
+      
+      
+    end
     
   end
-  
 end
 
 
 ## main
 if __FILE__ == $PROGRAM_NAME
 
-  command = Command.new(ARGV)
+  command = XMorgDown::Command.new(ARGV)
 
   command.parse()
   
   # (debug) Confirm options
-  if DEBUG
+  if XMorgDown::DEBUG
     print 'option: '
     pp command.option
   end
--- test_XMorgDown.rb
+++ test_XMorgDown.rb
@@ -9,77 +9,12 @@ require './XMorgDown.rb'
 
 class TestCommand < Test::Unit::TestCase 
   def setup
-    @argv = []
-    @command = []
 
-    # 0: true
-    c = %w(ruby XMorgDown.rb data/12.07.16_MOrgDown_idea.xmind)
-    @argv.push(c)
-    @command.push(Command.new(c))
-
-    # 1: true
-    c = %w(ruby XMorgDown.rb -f markdown data/12.07.16_MOrgDown_idea.xmind)
-    @argv.push(c)
-    @command.push(Command.new(c))
-
-    # 2: true
-    c = %w(ruby XMorgDown.rb -f org data/12.07.16_MOrgDown_idea.xmind)
-    @argv.push(c)
-    @command.push(Command.new(c))
-
-    # 3: false (throw exception)
-    c = %w(ruby XMorgDown.rb -f data/12.07.16_MOrgDown_idea.xmind)
-    @argv.push(c)
-    @command.push(Command.new(c))
-    
-    # 4: true
-    c = %w(ruby XMorgDown.rb data/12.07.16_MOrgDown_idea.xmind -f markdown)
-    @argv.push(c)
-    @command.push(Command.new(c))
-
-    # 5: true
-    c = %w(ruby XMorgDown.rb -h)
-    @argv.push(c)
-    @command.push(Command.new(c))
-
-    # 6: true
-    c = %w(ruby XMorgDown.rb -f org -o test.org data/12.07.16_MOrgDown_idea.xmind)
-    @argv.push(c)
-    @command.push(Command.new(c))
-
-    # 7: false (exit with error)
-    c = %w(ruby XMorgDown.rb -f markdown pandoc_help.txt)
-    @argv.push(c)
-    @command.push(Command.new(c))
     
   end
   
   def test_commands()
     
-    # # 0: true
-    # assert_nothing_thrown("0: success (true)") { @command[0].parse() }
-
-    # 1: true
-    pp @command[1].argv
-    assert_nothing_thrown("1: success (true)") { @command[1].parse() }
-
-    # # 2: true
-    # assert_nothing_thrown("2: success (true)") { @command[2].parse() }
-
-    # # 3: true
-    # assert_nothing_thrown("2: success (true)") { @command[3].parse() }
-    
-    # # 4: false (throw exception)
-    # assert_raise("4: success (throw exception)") { @command[4].parse() }
-
-    # # 5: true
-    # assert_nothing_thrown("5: success (true)") { @command[5].parse() }
-
-    # # 6: true
-    # assert_nothing_thrown("6: success (true)") { @command[6].parse() }
-
-    # # 7: false (exit with error)
-    # assert_raise("7: success (throw exception)") { @command[7].parse() }
     
   end
   
